abb726d6c3957a14d75e85be903e4098b0c20cfa
ryanchiang <ryanchiang@creer-design.com>
2024-06-27 09:18:14 +0800

    feat: score

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+// 評審輪播
+var swiper = new Swiper(".jurySwiper", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  // spaceBetween: 80,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    992: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    1340: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // mousewheel: true,
+  keyboard: true,
+});
